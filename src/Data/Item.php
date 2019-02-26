86304b723cdfe3e0a846a20cf83273d66f0e92d3
AllSecure Support <41504556+allsecure-pay@users.noreply.github.com>
2019-02-26 18:27:53 +0100

    Update Item.php

--- src/Data/Item.php
+++ src/Data/Item.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Ixopay\Client\Data;
+namespace Asx\Client\Data;
 
 /**
  * Class Item
  *
- * @package Ixopay\Client\Data
+ * @package Asx\Client\Data
  */
 class Item extends Data {
 
