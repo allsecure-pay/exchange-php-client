b9962260287bfaa0af3346265f33390dc71f7622
AllSecure Support <41504556+allsecure-pay@users.noreply.github.com>
2019-02-27 09:52:51 +0100

    Update ScheduleError.php

--- src/Schedule/ScheduleError.php
+++ src/Schedule/ScheduleError.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Ixopay\Client\Schedule;
+namespace Asx\Client\Schedule;
 
 /**
  * Class ScheduleError
  *
- * @package Ixopay\Client\Schedule
+ * @package Asx\Client\Schedule
  */
 class ScheduleError {
 
