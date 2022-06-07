d0688cc3e727f362ccc0247df05c9358289e4c9a
AllSecure <41504556+allsecure-pay@users.noreply.github.com>
2022-06-07 14:55:40 +0200

    Redirect QR-Code

--- src/Transaction/Result.php
+++ src/Transaction/Result.php
@@ -90,7 +90,12 @@ class Result {
      */
     protected $redirectUrl = null;
 
-    /**
+     /**
+     * @var string
+     */
+    protected $redirectQRCode = null;
+	
+	/**
      * @var string
      */
     protected $htmlContent = null;
@@ -182,6 +187,16 @@ class Result {
      */
     public function setRedirectUrl($redirectUrl) {
         $this->redirectUrl = $redirectUrl;
+        return $this;
+    }
+	
+	/**
+     * @param string $redirectQRCode
+     *
+     * @return $this
+     */
+    public function setRedirectQRCode($redirectQRCode) {
+        $this->redirectQRCode = $redirectQRCode;
         return $this;
     }
 
@@ -280,6 +295,15 @@ class Result {
      */
     public function getRedirectUrl() {
         return $this->redirectUrl;
+    }
+	
+	/**
+     * contains the QR Code (base64) with the redirect url if returnType = 'REDIRECT'
+     *
+     * @return string
+     */
+    public function getRedirectQRCode() {
+        return $this->redirectQRCode;
     }
 
     /**
